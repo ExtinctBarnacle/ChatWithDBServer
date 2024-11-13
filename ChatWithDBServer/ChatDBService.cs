aec22b8a48c43f862ccdc69718f41de79d4dfc09
Dmitry Antonov <dezzertir@yandex.ru>
2024-11-13 17:51:50 +0500

    Add project files.

---
+++ ChatWithDBServer/ChatDBService.cs
@@ -0,0 +1,59 @@
+﻿using ChatWithDBServer;
+using System;
+using System.Data.SQLite;
+
+class ChatDBService
+{
+    // Путь к файлу базы данных (создаст файл, если он не существует)
+    static string connectionString = "Data Source=mydatabase.db; Version=3;";
+    public static void CreateChatTable()
+    {
+        
+
+        // Создание базы данных и таблицы
+        using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+
+            // Создание таблицы
+            string createTableQuery = "CREATE TABLE IF NOT EXISTS Users (Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT)";
+            using (SQLiteCommand command = new SQLiteCommand(createTableQuery, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+    public static void StoreDataToDB(ChatMessage chatMessage) {
+        using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+            // Вставка данных
+            string insertQuery = "INSERT INTO Users (Name) VALUES (@name)";
+            using (SQLiteCommand command = new SQLiteCommand(insertQuery, connection))
+            {
+                command.Parameters.AddWithValue("@name", "Юлия");
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+    public static void LoadChatTable() {
+        using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+            // Чтение данных
+            string selectQuery = "SELECT * FROM Users";
+            using (SQLiteCommand command = new SQLiteCommand(selectQuery, connection))
+            {
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Id: {reader["Id"]}, Name: {reader["Name"]}");
+                    }
+                }
+            }
+        }
+    }
+        
+    
+}
---
+++ ChatWithDBServer/ChatServer.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Transactions;
+
+var tcpListener = new TcpListener(IPAddress.Any, 8080);
+//var words = new Dictionary<string, string>();
+try
+{
+    tcpListener.Start();    // запускаем сервер
+    Console.WriteLine("Сервер запущен. Ожидание подключений... ");
+    ChatDBService.CreateChatTable();
+
+    while (true)
+    {
+        // получаем подключение в виде TcpClient
+        using var tcpClient = await tcpListener.AcceptTcpClientAsync();
+        // получаем объект NetworkStream для взаимодействия с клиентом
+        var stream = tcpClient.GetStream();
+        // буфер для входящих данных
+        var response = new List<byte>();
+        int bytesRead = 10;
+        int msgCount = 0;
+        while (true)
+        {
+            // считываем данные до конечного символа
+            while ((bytesRead = stream.ReadByte()) != '\n')
+            {
+                // добавляем в буфер
+                response.Add((byte)bytesRead);
+            }
+            var word = Encoding.UTF8.GetString(response.ToArray());
+
+            // если прислан маркер окончания взаимодействия,
+            // выходим из цикла и завершаем взаимодействие с клиентом
+            if (word == "END") break;
+
+            Console.WriteLine($"Запрошен перевод слова {word}");
+            // находим слово в словаре и отправляем обратно клиенту
+            //if (!words.TryGetValue(word, out var translation)) translation = "не найдено в словаре";
+            msgCount++;
+            //words.Add(DateTime.Now.ToString() + " " + msgCount.ToString(), word);
+            Console.WriteLine(DateTime.Now.ToString() + word);
+            var translation = word + " message added to chat";
+            // добавляем символ окончания сообщения 
+            translation += '\n';
+            // отправляем перевод слова из словаря
+            await stream.WriteAsync(Encoding.UTF8.GetBytes(translation));
+            response.Clear();
+        }
+    }
+}
+finally
+{
+    tcpListener.Stop();
+}
